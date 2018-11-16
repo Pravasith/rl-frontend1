dfc7bdf6f828b2dfa44f38836ce9219dd4e0e41d
ninja <govind@rollinglogs.com>
2018-11-16 10:52:24 +0530

    vendor-product-dashboard

--- src/components/authComponents/profileDetailsVendor.js
+++ src/components/authComponents/profileDetailsVendor.js
@@ -775,7 +775,7 @@ class ProfileDetailsVendor extends React.Component {
 
                                                         <div className="inputColumn">
                                                             <input
-                                                                autoFocus="autofocus"
+                                                                // autoFocus="autofocus"
                                                                 type="text"
                                                                 placeholder="22"
                                                                 maxLength="2" 
