08aad3c536697274b2e467f99644aac0381742db
ninja <govind@rollinglogs.com>
2018-11-26 14:24:21 +0530

    vendor-dashboard-start-page ui complete

--- src/components/common/startPage.js
+++ src/components/common/startPage.js
@@ -1,10 +1,15 @@
 import React from "react"
 import Head from 'next/head'
+import Link from 'next/link'
 
 import '../../assets/sass/start_page.scss'
 
 import LogoAnimation from "../animations/logoAnimation"
 import Navbar from "../navbar/navbar"
+import { Footer } from "../footer/footer"
+
+import { FrontPageGraphic } from "../../assets/images";
+import { GradientButton } from "../UX/uxComponents"
 
 
 export default class StartPage extends React.Component {
@@ -52,8 +57,59 @@ export default class StartPage extends React.Component {
                             />
                         </div>
                     </header>
+                    <section className="mainContentSection">
+                        <div className="contentSectionInnerLayer">
+                            <header className="vendorMainHeaderSection">
+                                <h3>Showcase and sell your products/materials
+                                    to architects and interior designers</h3>
+                                <div className="line"></div>
+                            </header>
+                            <div className="architectureFrontContent">
+                                <div className="architectureFrontContentInnerLayer">
 
+                                    <div className="frontPageLeftSection">
+                                        <div className="svgImageInnerLayer">
+                                            <FrontPageGraphic />
+                                        </div>
+                                    </div>
+
+                                    <div className="frontPageRightSection">
+                                        <div className="rightSectionInnerLayer">
+                                            <div className="rightSectionContentWrapper">
+                                                <div className="rightSectionContentWrapperInnerLayer">
+                                                    <ul>
+                                                        <h3>
+                                                            <li>Build your profile, share it on social media</li>
+                                                        </h3>
+                                                        <h3>
+                                                            <li>Upload and manage your products on an
+                                                            amazingly user friendly dashboard</li>
+                                                        </h3>
+                                                        <h3>
+                                                            <li>Send quotes to architects, negotiate,
+                                                            deliver and get paid online!</li>
+                                                        </h3>
+                                                    </ul>
+                                                </div>
+                                                <div className="ProceedButtonContainer">
+                                                    <Link href="/register">
+                                                        <a >
+                                                            <GradientButton>
+                                                                Get started
+                                                            </GradientButton>
+                                                        </a>
+                                                    </Link>
+                                                </div>
+                                            </div> 
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </section>
+                <Footer />
             </article>
         )
     }
