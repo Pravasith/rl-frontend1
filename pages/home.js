e62e84a5c0a596e0d97e5628e459c822c3747f5e
pravasith <pravas@rollinglogs.com>
2019-01-03 19:20:24 +0530

    master merge

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 
-import "../src/assets/sass/sign_up_log_in.scss"
+import "../src/assets/sass/start_page.scss"
 import {Provider} from 'react-redux'
 import HomePage from "../src/components/common/homePage";
 import store from "../src/store"
