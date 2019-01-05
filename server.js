91aee3451154fde0daa0d90db3c1a475ed6f1e0e
pravasith <pravas@rollinglogs.com>
2019-01-05 13:34:52 +0530

    final deplyment

--- server.js
+++ server.js
@@ -2,22 +2,37 @@ const express = require('express')
 const next = require('next')
 const favicon = require('serve-favicon')
 var path = require('path')
+let https = require('https')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
+
+const fs = require('fs')
 
 
-
-
-app.prepare()
+nextApp.prepare()
     .then(() => {
         const server = express ()
 
-        // console.log(path.join(__dirname, "_next", "static", "/favicon.ico"))
-        
 
-        server.use(favicon(path.join(__dirname, "/favicon.ico")))
+        // let port = 8080;
+
+        let options = {
+            // key: fs.readFileSync('./ssl/privatekey.pem'),
+            // cert: fs.readFileSync('./ssl/certificate.pem'),
+            key: fs.readFileSync('/var/www/rollinglogs/fullkeys/privkey.pem', 'utf-8'),
+            cert: fs.readFileSync('/var/www/rollinglogs/fullkeys/fullchain.pem', 'utf-8')
+        };
+
+        // let app = express();
+
+        let app = https.createServer(options, server)
+        // .listen(port, function(){
+        // console.log("Express server listening on port " + port);
+        // });
+
+        // server.use(favicon(path.join(__dirname, "/favicon.ico")))
 
         // server.get('/vendor/profile-details/:id', (req, res) => {
         //     const actualPage = '/vendor-profile-details'
@@ -27,24 +42,24 @@ app.prepare()
 
         server.get('/vendor/profile-details', (req, res) => {
             const actualPage = '/vendor-profile-details'
-            app.render(req, res, actualPage)
+            nextApp.render(req, res, actualPage)
         })
 
         server.get('/vendor/dashboard', (req, res) => {
             const actualPage = '/vendor-main-dashboard'
-            app.render(req, res, actualPage)
+            nextApp.render(req, res, actualPage)
         })
 
         server.get('/vendor/add-product/:sCId', (req, res) => {
             const actualPage = '/vendor-add-product-details'
             const queryParams = { sCId: req.params.sCId }
-            app.render(req, res, actualPage, queryParams)
+            nextApp.render(req, res, actualPage, queryParams)
         })
 
         server.get('/vendor/edit-product/:pId', (req, res) => {
             const actualPage = '/vendor-edit-product-details'
             const queryParams = { pId: req.params.pId }
-            app.render(req, res, actualPage, queryParams)
+            nextApp.render(req, res, actualPage, queryParams)
         })
 
         server.get('*', (req, res) => {
