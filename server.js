74fe8fee81a1cab2c30666bac2b7cf11cb3df523
pravasith <pravas@rollinglogs.com>
2019-01-06 11:29:20 +0530

    shiz

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ nextApp.prepare()
         const server = express ()
 
 
-        let port = 80;
+        let port = 3000;
 
         let options = {
             // key: fs.readFileSync('./ssl/privatekey.pem'),
